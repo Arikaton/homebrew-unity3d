6a89e787c65f3cdb63118b637d07a0598185ab2c
Stanislav Ryzhov <stanislav.ryzhov@tripledotstudios.com>
2025-02-25 13:32:27 +0300

    Fix formula

--- Formula/unity3d.rb
+++ Formula/unity3d.rb
@@ -1,7 +1,7 @@
 class Unity3d < Formula
   desc "CLI tool for launching Unity projects with version detection"
   homepage "https://github.com/Arikaton/homebrew-unity3d"
-  url "https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/homebrew-unity3d/main/unity3d"
+  url "https://raw.githubusercontent.com/Arikaton/homebrew-unity3d/main/unity3d"
   version "1.0.0"
   sha256 "53a166b8096e35ead4f80aee4c5f74d2f0307266c5f0cbfb28dfc73cd9de7905"
 
